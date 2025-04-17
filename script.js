52ff1e9799b888218419f6b1cf24660012a2dd8a
AngieCaroP <karittopantoja@gmail.com>
2025-04-16 23:02:14 -0500

    responsive

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ function toggleMenu() {
     });
 
 
+  // Espera a que todo el DOM cargue
+  document.addEventListener('DOMContentLoaded', () => {
+    const hokoTitle = document.querySelector('.logo-title');
+    const menu = document.querySelector('.menu');
+
+    // Solo en móviles: alternar visibilidad al hacer clic en "HOKO"
+    hokoTitle.addEventListener('click', () => {
+      menu.classList.toggle('show');
+    });
+  });
